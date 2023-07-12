9777a69ca6129f0876aeb715374d52548fe7c2e5
jtm2021 <91266239+jtm2021@users.noreply.github.com>
2023-07-12 20:48:40 +0100

    Add useEffect hook for login credentials

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn"); // get the value of isLoggedIn from local storage. If it is not there, it will return null.
+
+    if (storedUserLoggedInInformation === "1") {
+      // if the value of isLoggedIn is 1, then set isLoggedIn to true.
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1"); // store in local storage. Here we are storing a string value of 1 for isLoggedIn. If you use 0, it will be false or not logged in.
     setIsLoggedIn(true);
   };
 
