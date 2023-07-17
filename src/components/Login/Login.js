4d478787c1a1c6a966359bea84158f906215c78d
jtm2021 <91266239+jtm2021@users.noreply.github.com>
2023-07-17 16:49:53 +0100

    Utilize useReducer hook for user email

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,37 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from "react";
 
-import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
+
+const emailReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.includes("@") }; // This is the new state.
+  }
+  if (action.type === "USER_BLUR") {
+    return { value: state.value, isValid: state.value.includes("@") }; // This is the new state.
+  }
+  return { value: '', isValid: false };
+};
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
+  // const [enteredEmail, setEnteredEmail] = useState(""); // We omit this because we utilize useReducer below.
+  // const [emailIsValid, setEmailIsValid] = useState(); // We omit this because we utilize useReducer below.
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: false,
+  });
+
   useEffect(() => {
-    console.log('EFFECT RUNNING'); // This is just to show that useEffect runs after every render cycle. It is not necessary to have this here.
+    console.log("EFFECT RUNNING"); // This is just to show that useEffect runs after every render cycle. It is not necessary to have this here.
   }, []); // This is a dependency array. If you leave it empty, then it will run only once. If you put a variable in it, then it will run when that variable changes. If you put nothing, then it will run after every render cycle.
 
-  useEffect(() => {
-    const identifier = setTimeout(() => {
-      console.log('Checking form validity!');
-      setFormIsValid(
-        enteredEmail.includes('@') && enteredPassword.trim().length > 6
-      );
-    }, 500); // This is just to show that useEffect runs after every render cycle. It is not necessary to have this here.
-    
-    return () => {
-      console.log('CLEANUP');
-      clearTimeout(identifier); // This is just to show that useEffect runs after every render cycle.
-    }; // This is a clean up function. It runs before the useEffect runs again. It is not necessary to have this here.
-  }, [enteredEmail, enteredPassword]); // enteredEmail and enteredPassword are dependencies. If either of these change, then the useEffect will run.
+  // useEffect(() => {
+  //   const identifier = setTimeout(() => {
+  //     console.log("Checking form validity!");
+  //     setFormIsValid(
+  //       enteredEmail.includes("@") && enteredPassword.trim().length > 6
+  //     );
+  //   }, 500); // This is just to show that useEffect runs after every render cycle. It is not necessary to have this here.
+
+  //   return () => {
+  //     console.log("CLEANUP");
+  //     clearTimeout(identifier); // This is just to show that useEffect runs after every render cycle.
+  //   }; // This is a clean up function. It runs before the useEffect runs again. It is not necessary to have this here.
+  // }, [enteredEmail, enteredPassword]); // enteredEmail and enteredPassword are dependencies. If either of these change, then the useEffect will run.
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({ type: "USER_INPUT", val: event.target.value });
 
+    setFormIsValid(
+      event.target.value.includes("@") && enteredPassword.trim().length > 6
+    );
     // setFormIsValid( // This has been removed from here and put in useEffect above.
     //   event.target.value.includes('@') && enteredPassword.trim().length > 6
     // );
@@ -40,22 +58,28 @@ const Login = (props) => {
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
 
+    setFormIsValid(
+      emailState.isValid && event.target.value.trim().length > 6
+    );
+
     // setFormIsValid( // This has been removed from here and put in useEffect above.
     //   event.target.value.trim().length > 6 && enteredEmail.includes('@')
     // );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({ type: "USER_BLUR" });
+    // setEmailIsValid(emailState.isValid);
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    // dispatchEmail({ type: "USER_BLUR" });
+    setPasswordIsValid(enteredPassword.trim().length > 6); // We omit this and replace it with dispatchEmail above.
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -63,21 +87,21 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
