9a35162694f001c72567c9e3ca54562f20bc2e4e
jtm2021 <91266239+jtm2021@users.noreply.github.com>
2023-07-18 22:11:24 +0100

    Utilize useReducer hook for password

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -14,17 +14,31 @@ const emailReducer = (state, action) => {
   return { value: '', isValid: false };
 };
 
+const passwordReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.trim().length > 6 }; // This is the new state.
+  }
+  if (action.type === "USER_BLUR") {
+    return { value: state.value, isValid: state.value.trim().length > 6 }; // This is the new state.
+  }
+  return { value: '', isValid: false };
+};
+
 const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState(""); // We omit this because we utilize useReducer below.
   // const [emailIsValid, setEmailIsValid] = useState(); // We omit this because we utilize useReducer below.
-  const [enteredPassword, setEnteredPassword] = useState("");
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     value: "",
-    isValid: false,
+    isValid: null,
   });
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: "",
+    isValid: null,
+  })
 
   useEffect(() => {
     console.log("EFFECT RUNNING"); // This is just to show that useEffect runs after every render cycle. It is not necessary to have this here.
@@ -48,7 +62,7 @@ const Login = (props) => {
     dispatchEmail({ type: "USER_INPUT", val: event.target.value });
 
     setFormIsValid(
-      event.target.value.includes("@") && enteredPassword.trim().length > 6
+      event.target.value.includes("@") && passwordState.isValid
     );
     // setFormIsValid( // This has been removed from here and put in useEffect above.
     //   event.target.value.includes('@') && enteredPassword.trim().length > 6
@@ -56,7 +70,8 @@ const Login = (props) => {
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+    dispatchPassword({ type: "USER_INPUT", val: event.target.value });
+    // setEnteredPassword(event.target.value); // We omit this because we utilize useReducer above.
 
     setFormIsValid(
       emailState.isValid && event.target.value.trim().length > 6
@@ -73,13 +88,14 @@ const Login = (props) => {
   };
 
   const validatePasswordHandler = () => {
+    dispatchPassword({ type: "USER_BLUR" });
     // dispatchEmail({ type: "USER_BLUR" });
-    setPasswordIsValid(enteredPassword.trim().length > 6); // We omit this and replace it with dispatchEmail above.
+    // setPasswordIsValid(enteredPassword.trim().length > 6); // We omit this and replace it with dispatchEmail above.
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, enteredPassword);
+    props.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -101,14 +117,14 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ""
+            passwordState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
